306c106c3159313aa097dc2492d86c0f19493797
Arthur <car3man@gmail.com>
2024-01-08 03:13:20 +0300

    Example update

--- example/example.js
+++ example/example.js
@@ -5,7 +5,7 @@ const documentPath = join(__dirname, "./example.collection");
 const document = readFileSync(documentPath, "utf-8");
 const tree = deserialize(document);
 const objects = tree.getNodes().filter((node) => node.getKey() === "myObject");
-const objectToFind = objects.filter((myObject) => {
+const object = objects.find((myObject) => {
    return myObject.getNodes().some((child) => child.getKey() === "name" && child.getValue() === "Skuf"); 
-})[0];
-console.log(objectToFind);
+});
+console.log(object);
